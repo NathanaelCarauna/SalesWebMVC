4c582ae3077c2cb77285cd651204cd99e2248f93
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-06-06 16:16:45 -0300

    First model-view-controller -Department

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
